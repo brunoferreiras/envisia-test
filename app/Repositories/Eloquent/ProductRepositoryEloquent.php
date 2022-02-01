2df20b8071bbfc885ba53d44f84f4b8adffaab8c
Bruno Ferreira <fs.brunoferreira@gmail.com>
2022-01-31 22:43:23 -0300

    feat: add product repository

---
+++ app/Repositories/Eloquent/ProductRepositoryEloquent.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use Prettus\Repository\Eloquent\BaseRepository;
+use Prettus\Repository\Criteria\RequestCriteria;
+use App\Repositories\ProductRepository;
+use App\Models\Product;
+
+/**
+ * Class ProductRepositoryEloquent.
+ *
+ * @package namespace App\Repositories\Eloquent;
+ */
+class ProductRepositoryEloquent extends BaseRepository implements ProductRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return Product::class;
+    }
+
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
+
+}
