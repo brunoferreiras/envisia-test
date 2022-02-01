8969a664c6c6205a5575ac4e53da535b7a11a2e7
Bruno Ferreira <fs.brunoferreira@gmail.com>
2022-01-31 22:39:16 -0300

    feat: add customer repository

---
+++ app/Models/Customer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+/**
+ * Class Customer.
+ *
+ * @package namespace App\Models;
+ */
+class Customer extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['first_name', 'last_name', 'email', 'phone', 'birthday', 'gender', 'image'];
+}
---
+++ app/Repositories/Eloquent/CustomerRepositoryEloquent.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use Prettus\Repository\Eloquent\BaseRepository;
+use Prettus\Repository\Criteria\RequestCriteria;
+use App\Repositories\CustomerRepository;
+use App\Models\Customer;
+
+/**
+ * Class CustomerRepositoryRepositoryEloquent.
+ *
+ * @package namespace App\Repositories\Eloquent;
+ */
+class CustomerRepositoryEloquent extends BaseRepository implements CustomerRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return Customer::class;
+    }
+
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
+
+}
